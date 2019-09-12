7787d18746be0e05bb735594149ccb87404f3e8e
Christian Brito Ramos <christian.brito.ramos@gmail.com>
2019-09-12 11:47:51 +0100

    hanging done

--- ahorcado.py
+++ ahorcado.py
@@ -46,12 +46,12 @@ def redraw_game_window():
             label = btn_font.render(chr(buttons[i][5]), 1, BLACK)
             window.blit(label, (buttons[i][1] - (label.get_width() / 2), buttons[i][2] - (label.get_height() / 2)))
 
-    ##spaced = spacedOut(word, guessed)
-    ##label1 = guess_font.render(spaced, 1, BLACK)
-    ##rect = label1.get_rect()
-    ##length = rect[2]
+    spaced = spacedOut(word, guessed)
+    label1 = guess_font.render(spaced, 1, BLACK)
+    rect = label1.get_rect()
+    length = rect[2]
     
-    ##window.blit(label1,(winWidth/2 - length/2, 400))
+    window.blit(label1,(winWidth/2 - length/2, 400))
     pygame.draw.rect(window,(0, 153, 51),(winWidth/2-100,winHeight/2-100,230,250))
     pic = hangmanPics[limbs]
     window.blit(pic, (winWidth/2 - pic.get_width()/2 + 20, 150))
@@ -62,6 +62,37 @@ def randomWord():
     f = file.readlines()
     i = random.randrange(0,len(f)-1)
     return f[i][:-1]
+
+def buttonHit(x , y):
+    for i in range(len(buttons)):
+        if x < buttons[i][1]+20 and x > buttons[i][1]-20:
+            if y < buttons[i][2]+20 and y > buttons[i][2]-20:
+                return buttons[i][5]
+    return None
+
+def hang(guess):
+    global word
+    if guess.lower() not in word.lower():
+        return True
+    else:
+        return False
+
+def end():
+    pygame.quit()
+
+def spacedOut(word, guessed=[]):
+    spacedWord = ''
+    guessedLetters = guessed
+    for x in range(len(word)):
+        if word[x] != ' ':
+            spacedWord += '_ '
+            for i in range(len(guessedLetters)):
+                if word[x].upper() == guessedLetters[i]:
+                    spacedWord = spacedWord[:-2]
+                    spacedWord += word[x].upper() + ' '
+        elif word[x] == ' ':
+            spacedWord += ' '
+    return spacedWord
 
 #########################################################
 ## Setup buttons.                                      ##
@@ -79,6 +110,7 @@ for i in range(26):
     # buttons.append([color, x_pos, y_pos, radius, visible, char])
 
 word = randomWord()
+print(word)
 inPlay = True
 
 while inPlay:
@@ -93,7 +125,7 @@ while inPlay:
                 inPlay = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             clickPos = pygame.mouse.get_pos()
-            #letter = buttonHit(clickPos[0], clickPos[1])
+            letter = buttonHit(clickPos[0], clickPos[1])
             if letter != None:
                 guessed.append(chr(letter))
                 buttons[letter - 65][4] = False
